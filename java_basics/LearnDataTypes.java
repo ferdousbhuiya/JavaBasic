3ff5fc85513b4b76a773ef032f7d8be41f359b0f
ferdousbhuiya <ferdousbhuiya@gmail.com>
2022-09-25 01:01:57 -0400

    This is first commit.

---
+++ java_basics/LearnDataTypes.java
@@ -0,0 +1,10 @@
+package java_basics;
+
+public class LearnDataTypes {
+
+    public static void minMethod(String[] arg) {
+        int age =56;
+
+        System.out.println(age);
+    }
+}
---
+++ java_basics/LearnJava.java
@@ -0,0 +1,29 @@
+package java_basics;
+
+public class LearnJava {
+
+/*
+2 things inside of every class
+1.Attributes/variables
+2. Functions/Methods
+ */
+
+    String name = "variables"; //this is Global variables, outside the method but inside the class
+public void methodOne(String[] arg){
+
+    System.out.println("name");
+
+    public void methodTwo(){
+
+
+        System.out.println();
+    }
+    public void methodThree(){
+
+
+        System.out.println();
+    }
+
+
+}
+}
